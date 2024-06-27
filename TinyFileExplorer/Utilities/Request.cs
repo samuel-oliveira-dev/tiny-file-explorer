3c361ca30c5c926a7eb919b6f006a09f85012876
Samuel dos Santos Oliveira <77649064+samuel-oliveira-dev@users.noreply.github.com>
2024-06-26 23:11:35 -0300

    upload de arquivos de arquivos iniciado

--- TinyFileExplorer/Utilities/Request.cs
+++ TinyFileExplorer/Utilities/Request.cs
@@ -1,8 +1,11 @@
-﻿namespace TinyFileExplorer.Utilities
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TinyFileExplorer.Utilities
 {
     public class Request
     {
         public string Path {  get; set; }
         public string Action { get; set; }
+        public IFormFile Files { get; set; }
     }
 }
